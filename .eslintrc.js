3b8bb519e610db0fb3392e77ae0c962126988b6f
Purwa Sadr <purwasadr@gmail.com>
2023-04-17 21:23:52 +0700

    chore: add dependencies and configuration

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ["unused-imports"],
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
